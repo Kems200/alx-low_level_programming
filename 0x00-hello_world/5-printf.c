13cb2c415420a91e08e9e42b2ab45947ff4faab2
kems200 <nkemjikankemjika2001@gmail.com>
2022-09-08 09:57:29 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the put fuction
+ *
+ * Description: using the main fuction
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n*);
+	return (0);
+}
+
